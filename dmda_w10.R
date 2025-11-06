4be3686cb9b58a935475d00bf94aa8db5ba3bc4d
sindhu-6 <sindhugoudyaragani@gmail.com>
2025-11-06 23:08:11 +0530

    Update dmda_w10.R

--- dmda_w10.R
+++ dmda_w10.R
@@ -30,13 +30,11 @@ library(caret)
 library(C50)
 set.seed(7)
 inTraininglocal<-createDataPartition(iris$Species,p=.70,list = F)
-inTraininglocal
 training<-iris[inTraininglocal,]
 testing<-iris[-inTraininglocal,]
-model<-C5.0(Species~.,data = training) 
+model<-C5.0(Species~.,data = training)
 summary(model)
-pred<-predict.C5.0(model,testing[,-5])
-pred
+pred<-predict.C5.0(model,testing[,-5]) #type ="prob"
 a<-table(testing$Species,pred)
 sum(diag(a))/sum(a)
 plot(model)
